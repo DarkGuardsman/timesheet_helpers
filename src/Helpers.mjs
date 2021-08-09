9b08987ef57794b6b7af4b534f869037433c585b
Robin Seifert <rseifert.phone@gmail.com>
2021-08-09 07:30:10 -0400

    Added null & undefined to time formatter

--- src/Helpers.mjs
+++ src/Helpers.mjs
@@ -1,6 +1,9 @@
 
 export function formatTime(time) {
-    if(time !== '-' && time !== '') {
+    if(time === undefined || time === null) {
+        console.error("Invalid input: " + time)
+    }
+    else if(time !== '-' && time !== '') {
         const ending = time.substring(time.length - 2, time.length);
 
         return time.substring(0, time.length - 2) + ":00 " + ending.toUpperCase();
